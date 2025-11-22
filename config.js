cc227c8efd9ad5e40a39f135fc537464b6276c0d
DominicDev <s0l0estudi4r@gmail.com>
2025-11-22 16:33:46 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'LOREEN';
+export const nameSongs = [
+	"TATTOO", /*1*/
+	"GRAVITY", /*2*/
+	"EUPHORIA", /*3*/
+	"71 CHARGER", /*4*/
+	"DREAMS", /*5*/
+	"JUPITER DRIVE", /*6*/
+	"FIRE BLUE", /*7*/
+	"HATE THE WAY I LOVE YOU", /*8*/
+	"I GO EGO", /*9*/
+	"HEART ON HOLD", /*10*/
+	"LOVE ME AMERICA", /*11*/
+	"RIDE", /*12*/
+	"IN MY HEAD", /*13*/
+	"MY HEART IS REFUSING ME", /*14*/
+	"EVERYTIME", /*15*/
+	"CRYING OUT YOUR NAME", /*16*/
+	"DO WE EVEN MATTER", /*17*/
+	"SIDEWALK", /*18*/
+	"SOBER", /*19*/
+	"JUNGLE", /*20*/
+]
